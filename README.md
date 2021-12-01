4c17c6c85e8a2887a635e5f4bd4c8e3e929ab7fe
Geatfine <david.houssian@gmail.com>
2021-12-01 21:10:53 +0100

    Add live badges
    Fix badge
    
    Fix badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # maven_training
+![example workflow](https://github.com/Geatfine/maven_training/actions/workflows/build.yml/badge.svg)
+[![codecov.io](https://codecov.io/gh/Geatfine/maven_training/coverage.svg)](https://codecov.io/gh/Geatfine/maven_training)
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -8,13 +8,13 @@ class SampleTest {
 
     @Test
     void op() {
-        assertEquals(7, new Sample().op(Sample.Operation.ADD, 3, 4));
-        assertEquals(12, new Sample().op(Sample.Operation.MULT, 3, 4));
+        assertEquals(8, new Sample().op(Sample.Operation.ADD, 4, 4));
+        assertEquals(16, new Sample().op(Sample.Operation.MULT, 4, 4));
     }
 
     @Test
     void fact() {
         assertEquals(6, new Sample().fact(3));
-        assertEquals(720, new Sample().fact(6));
+        assertEquals(24, new Sample().fact(4));
     }
 }
